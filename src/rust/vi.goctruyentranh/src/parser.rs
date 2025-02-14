ac41c744c5d7a67b88fc085c9d4112634b7ba826
Gnakclone1507 <pnkhangclone10@gmail.com>
2025-02-14 16:31:48 +0700

    Update

--- src/rust/vi.goctruyentranh/src/parser.rs
+++ src/rust/vi.goctruyentranh/src/parser.rs
@@ -1,31 +1,28 @@
-use aidoku_std::prelude::*;
-use aidoku_std::net::Request;
-use aidoku_std::html::HtmlNode;
-use aidoku_std::chapter::{Chapter, ChapterList};
-use aidoku_std::manga::{Manga, MangaList};
-use aidoku_std::page::{Page, PageList};
+use aidoku::prelude::*;
+use aidoku::std::net::Request;
+use aidoku::std::String;
+use aidoku::std::Vec;
+use aidoku::Manga;
+use aidoku::Chapter;
+use aidoku::Page;
 
-const BASE_URL: &str = "https://goctruyentranhvuiaa.com";
+pub fn get_manga_list() -> Vec<Manga> {
+    let html = Request::new("https://goctruyentranhvuiaa.com/danh-sach-truyen", Method::Get)
+        .html();
+    // Phân tích HTML để lấy danh sách truyện
+    Vec::new()
+}
 
-pub fn get_manga_list() -> MangaList {
-    let url = format!("{}/danh-sach-truyen", BASE_URL);
-    let html = Request::new(&url).html().unwrap();
-    let mut manga_list = Vec::new();
+pub fn get_chapter_list(manga_id: String) -> Vec<Chapter> {
+    let url = format!("https://goctruyentranhvuiaa.com/{}", manga_id);
+    let html = Request::new(&url, Method::Get).html();
+    // Phân tích HTML để lấy danh sách chương
+    Vec::new()
+}
 
-    for item in html.select(".manga-list .item") {
-        let title = item.select(".manga-title").text().unwrap();
-        let cover_url = item.select(".manga-cover img").attr("src").unwrap();
-        let link = item.select("a").attr("href").unwrap();
-        manga_list.push(Manga {
-            title: title.to_string(),
-            cover_url: cover_url.to_string(),
-            link: link.to_string(),
-            ..Default::default()
-        });
-    }
-
-    MangaList {
-        manga: manga_list,
-        has_more: false,
-    }
+pub fn get_page_list(chapter_id: String) -> Vec<Page> {
+    let url = format!("https://goctruyentranhvuiaa.com/{}", chapter_id);
+    let html = Request::new(&url, Method::Get).html();
+    // Phân tích HTML để lấy danh sách trang ảnh
+    Vec::new()
 }
